99d14e3f17c2055b32521bc369b4503127269cfe
Mozocago <djotchip@gmail.com>
2025-03-25 13:36:01 +0100

    Create quizcontrole.js

---
+++ quizcontrole.js
@@ -0,0 +1,5 @@
+<div class="boutons-fixes">
+        <button class="boutonn" onclick="precedent()">⬅️ Précédent</button>
+        <button class="bouton" onclick="suivant()">➡️ Suivant </button>
+        <button class="bouton jaime" onclick="aimer()">❤️ J'aime</button>
+    </div>
